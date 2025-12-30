254db41f1c90bd7e303d38962f08e4cd7cf43d9e
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2025-12-30 20:40:44 +0545

    attributes shape

---
+++ 2-numpy_array_propeties/shape.py
@@ -0,0 +1,6 @@
+
+import numpy as np   
+
+array_2d =  np.array([[1,2,3],
+                       [4,5,6]])
+print(array_2d.shape)
--- firs_numpy.py/1d.py
+++ firs_numpy.py/1d.py
@@ -3,3 +3,6 @@ import numpy as np
 ar_1d  = np.array([10,30,39,50,39])
 
 print(ar_1d)
+
+
+import qrcode
