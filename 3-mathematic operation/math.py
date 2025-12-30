8cfca906e5547a86c955fa7bdd358a5970d45bbf
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2025-12-30 21:14:34 +0545

    agg_function

---
+++ 3-mathematic operation/math.py
@@ -0,0 +1,7 @@
+import numpy as np   
+
+arr  = np.array([1,2,3,4])
+print (arr + 5)
+print (arr - 1)
+print (arr * 5)
+print (arr /1)
---
+++ 4- agg_function/agg.py
@@ -0,0 +1,10 @@
+import numpy as np  
+arr  = np.array([1,2,3,4,5,6])
+print(np.sum(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.mean(arr))
+print(np.median(arr))
+print(np.var(arr))
+print(np.std(arr))
+
