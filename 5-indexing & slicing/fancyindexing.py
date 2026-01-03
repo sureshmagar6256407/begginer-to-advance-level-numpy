f44f784336d04d71fb11d1e4be64f3113e5fbf29
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-03 15:36:05 +0545

    fancyslicing

--- 5-indexing & slicing/fancyindexing.py
+++ 5-indexing & slicing/fancyindexing.py
@@ -2,3 +2,10 @@
 fancy index --> selecting multiple element ones 
 """
 
+import numpy as np  
+arr  = np.array([1,2,3,4,5]) 
+print(arr[[0,3]])#ints use for 1d 
+
+arr1  = np.array([[2,3,4,5],
+                  [3,4,5,6]]) 
+print(arr1[1 ,[1,3]]) #its user for multidimensinal
