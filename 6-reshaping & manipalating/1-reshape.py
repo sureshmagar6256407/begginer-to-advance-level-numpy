3f0021af0ff164fea9a2d36276e9c86b2b85da53
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-22 16:17:09 +0545

    updtae

---
+++ 6-reshaping & manipalating/1-reshape.py
@@ -0,0 +1,7 @@
+"""
+Docstring for 6-reshaping & manipalating.1-reshape
+reshap[rows, columns] specify new shape 
+"""
+import numpy as np   
+arr  = np.array ([1,2,3,4,5,6])
+print (arr.reshape(3,2))
