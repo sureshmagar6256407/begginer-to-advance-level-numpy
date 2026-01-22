e87d7b37de262953315e8ee58a41d3720c2b542d
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-22 17:28:59 +0545

    something practice on split

--- 7-advanced_numpy/7-spliting.py
+++ 7-advanced_numpy/7-spliting.py
@@ -6,5 +6,15 @@ np.vsplit(array, indices_or_sections)
 """
 
 import numpy as np 
-arr  = np.array ([10,30,49,50,60,90])
-print (np.split(arr, 2))
+# arr  = np.array ([10,30,49,50,60,90])
+# print (np.split(arr, 2))
+# print (np.hsplit(arr,2))
+
+
+arr_2d  = np.array([[1,2,3,11] , 
+                    [4,5,6,12] , 
+                    [7,8,9,10]])
+print (np.hsplit(arr_2d,2))
+print (np.vsplit(arr_2d,3))
+print (np.split(arr_2d,3 ,axis= 0))
+print (np.split(arr_2d , 2 ,axis =1 ))
---
+++ 7-advanced_numpy/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+import numpy as np 
+# arr  = np.array ([10,30,49,50,60,90])
+# print (np.split(arr, 2))
+# print (np.hsplit(arr,2))
+
