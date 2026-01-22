e93cac562e354174b556b58352f2134418ff2e4a
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-22 16:35:05 +0545

    update

---
+++ 7-advanced_numpy/1-insert.py
@@ -0,0 +1,13 @@
+"""
+Docstring for 7-advanced_numpy.1-insert
+np.insert(array,index,value,axis=None)
+array  - original array  
+index  - position where the new value is inserted
+value  - the value to be inserted
+axis - axis along which to insert the new value (default is None, which flattens the array)
+"""
+import numpy as np  
+arr = np.array ([10,30,29,40,29])
+print(arr)
+new_arr  = np.insert(arr ,2,50,axis=None)
+print(new_arr)
