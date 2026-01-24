17771977119e1d0056f8cade8f3a4f85866acf2f
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-24 14:58:35 +0545

    vectorization

--- 8-BrodCasting/4-error.py
+++ 8-BrodCasting/4-error.py
@@ -4,5 +4,7 @@ arr1  = np.array ([[1,2,3] , [
     4,5,6
 ] ])
 arr2 = np.array([1,2])
-result = arr1 + arr2 
+res  = arr2.reshape (2,1) 
+print(res)
+result = arr1 + res 
 print(result)
---
+++ 9-vectrization/2-fast.py
@@ -0,0 +1,5 @@
+import numpy as np 
+ar1   = np.array ([1,2,3] )  
+arr2 = np.array ([4,5,6])  
+result = ar1  + arr2  
+print(result)
